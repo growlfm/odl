64f1cd8534ba235c827718051c611cf0c8735a1d
chrishic <chris@chrishic.com>
2022-09-29 14:59:31 -0700

    When building UA db, only add regex for empty string once

--- odl/players.py
+++ odl/players.py
@@ -5,8 +5,6 @@ import time
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-__all__ = ["get_app"]
-
 
 def get_useragents():
     with open(os.path.join(DIR_PATH, './data/user-agents.json'),
@@ -21,6 +19,7 @@ class Players(object):
 
     https://github.com/opawg/user-agents
     """
+
     _db = None
 
     def get(self, user_agent):
@@ -29,7 +28,6 @@ class Players(object):
 
             # Create a db of (re, {data})
             for row in get_useragents():
-
                 for ua in row['user_agents']:
                     self._db.append((re.compile(ua), {
                         "app": row.get("app", "Unknown"),
@@ -38,22 +36,22 @@ class Players(object):
                         "bot": row.get("bot", False),
                     }))
 
-                # We assume anything without a user_agent is a bot.
-                self._db.append((re.compile('^(|\s+)$'), {
-                    "app": "Unknown",
-                    "device": "Unknown",
-                    "os": "Unknown",
-                    "bot": True
-                }))
+            # We assume anything without a user_agent is a bot.
+            self._db.append((re.compile('^(|\s+)$'), {
+                "app": "Unknown",
+                "device": "Unknown",
+                "os": "Unknown",
+                "bot": True
+            }))
 
-        # Searh the db for
+        # Search the db for
         for ua_re, data in self._db:
             if ua_re.search(user_agent):
                 resp = data.copy()
                 resp["user_agent"] = user_agent
                 return resp
 
-        # by default we don't want to trash unknown agents.
+        # Don't trash unknown agents
         return {
             "user_agent": user_agent,
             "app": "Unknown",
